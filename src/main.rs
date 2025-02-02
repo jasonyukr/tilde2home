a2e1c2bff3819c953564e3e89bced838a5dc204b
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2025-02-02 18:24:20 +0900

    Use strip-ansi-escapes

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,19 @@ fn main() {
     // Get the user's home directory
     let home_dir = env::var("HOME").unwrap();
 
-    // Read from stdin
     let stdin = io::stdin();
-    let handle = stdin.lock();
-
-    for line in handle.lines() {
-        match line {
-            Ok(mut line) => {
-                // Find the position of the home directory in the line
-                if let Some(pos) = line.find("~") {
-                    if pos == 0 {
-                        // Replace "~" with home directory
-                        line = line.replacen("~", &home_dir, 1);
-                    }
+    for line in stdin.lock().lines() {
+        if let Ok(mut ln) = line {
+            let strip_ln = strip_ansi_escapes::strip_str(ln.clone());
+            // Find the position of the home directory in the line
+            if let Some(pos) = strip_ln.find("~") {
+                if pos == 0 {
+                    // Replace "~" with home directory
+                    ln = ln.replacen("~", &home_dir, 1);
                 }
-                // Print the line, preserving escape sequences
-                println!("{}", line);
             }
-            Err(_e) => {}
+            // Print the line, preserving escape sequences
+            println!("{}", ln);
         }
     }
 }
